e22783369e0d5088bce171c330f7d4ecaebf12ef
Aiden Cary <ACARY@cub.uca.edu>
2026-03-29 19:29:13 -0500

    fix(cors): allow all headers in CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_headers=["*"],
 )
 
 
